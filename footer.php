952773d95898ecd3f3cbac0382446fbac2e77e49
髙森涼 <takamori_ryo@manabito.biz>
2021-08-05 21:15:07 +0900

    0805作業分

--- footer.php
+++ footer.php
@@ -2,6 +2,8 @@
 <footer>
 	<small>&copy; MdN Corporation.</small>
 </footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
