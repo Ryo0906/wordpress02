6c60f6290ad5147232cb688c747a90f18c75144e
髙森涼 <takamori_ryo@manabito.biz>
2021-08-19 21:02:18 +0900

    8月19日作業分

--- header.php
+++ header.php
@@ -1,41 +1,42 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php echo esc_attr( bloginfo( 'charset' ) ); ?>">
-	<meta name="viewport" content="width=device-width">
+  <meta charset="<?php echo esc_attr( bloginfo( 'charset' ) ); ?>">
+  <meta name="viewport" content="width=device-width">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class( 'top' ); ?>>
 <header>
-	<div class="header-logo">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-			<img src="img/logo.svg" alt="Go! CAMP" width="240">
-      <?php
-      if ( has_custom_logo() ) {
-        $custom_logo_id = get_theme_mod( 'custom_logo' );
-        $image = wp_get_attachment_image_src( $custom_logo_id, 'full' );
-        var_dump( $image );
-      }
-      ?>
-		</a>
-	</div>
-	<button id="hamburger" class="header-button">
-		<span class="icon"></span>
-	</button>
-	<nav class="header-gnav">
-		<ul>
-			<li>
-				<a href="index.html">トップページ</a>
-			</li>
-			<li>
-				<a href="list.html">NEWS</a>
-			</li>
-			<li>
-				<a href="faq.html">よくある質問</a>
-			</li>
-			<li>
-				<a href="contact.html">お問い合わせ</a>
-			</li>
-		</ul>
-	</nav>
+  <div class="header-logo">
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<?php
+			if ( has_custom_logo() ) {
+				$custom_logo_id = get_theme_mod( 'custom_logo' );
+				$image = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+				$html = '<img src="' . $image[0] . '"';
+				$html .= ' width="' . $image[1] . '';
+				$html .= ' height="' . $image[2] . '"';
+				$html .= ' alt="' . esc_attr( get_bloginfo( 'name' ) ) . '">';
+				echo $html;
+			}
+			?>
+    </a>
+  </div>
+	<?php if( has_nav_menu( 'global' ) ) : ?>
+      <button id="hamburger" class="header-button">
+        <span class="icon"></span>
+      </button>
+      <nav class="header-gnav">
+        <?php
+        $args = array(
+          // container falseで囲っていたdivタグを消す
+          'container'      => false,
+          // items_wrap　でulタグ内についていた不要なidとclassを消す
+          'items_wrap'      => '<ul>%3$s</ul>',
+          'theme_location' => 'global'
+        );
+        wp_nav_menu( $args );
+        ?>
+      </nav>
+	<?php endif; ?>
 </header>
