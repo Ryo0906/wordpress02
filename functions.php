1c85872e58f15c44b7adba4209ab598558c68e4e
Ryo0906 <86959395+Ryo0906@users.noreply.github.com>
2021-09-17 12:29:43 +0900

    0916

--- functions.php
+++ functions.php
@@ -2,6 +2,19 @@
 function theme_setup()  {
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'custom-logo' );
+	add_theme_support( 'post-thumbnails' );
+
+	add_theme_support(
+		'post-formats',
+		array(
+			'image'
+		)
+	);
+
+	register_nav_menus( array(
+		'global' => 'Global Menu'
+	) );
+
 }
 add_action( 'after_setup_theme', 'theme_setup' );
 
